410f030c63cb335e48be4e597fd3a8dd87c507a8
Valhe Kapil Ravindra <krwalhe007@gmail.com>
2021-10-17 15:08:30 +0530

    Final

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -79,5 +79,18 @@ public class StringCalculatorShould {
 		assertThat(StringCalculator.Add("1002"), is(2));
 		assertThat(StringCalculator.Add("1040,10002"), is(42));
 	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void acceptsDelimiterOfArbitraryLength() {
+		assertThat(StringCalculator.Add("//[***]\n1***2***3"), is(6));
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void acceptsMultipleDelimiters() {
+		assertThat(StringCalculator.Add("//[-][;]\n1-2;3"), is(6));
+		assertThat(StringCalculator.Add("//[--][...]\n2--3...4"), is(9));
+	}
 
 }
