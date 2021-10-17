19e3453060e035a0b2d4eaa02b02024a56cba7f8
Valhe Kapil Ravindra <krwalhe007@gmail.com>
2021-10-17 11:32:44 +0530

    First Case

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,54 +1,65 @@
 package calculator;
 
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class StringCalculator {
 
-    public int add(String input) {
-		
-        if(input.equals("")){
-			return 0;
-		}
-		else{
-			String delimiter = ",";
-			if(input.matches("//(.)\n(.)")){
-				delimiter = Character.toString(input.charAt(2));
-				input = input.substring(4);
-			}
-			
-			String numList[] = splitNumbers(input, delimiter + "|\n");
-			return sum(numList);
-		}
-    }
-    
-    private  String[] splitNumbers(String numbers, String divider){
-	    return numbers.split(divider);
-	}
-    
-    private  int toInt(String number){
-		return Integer.parseInt(number);
+	private String delimiter;
+	private String numbers;
+	private StringCalculator(String delimiter, String numbers) {
+		this.delimiter = delimiter;
+		this.numbers = numbers;
 	}
 
-    
-    private  int sum(String[] numbers){
- 	    int total = 0;
- 	    String negString = "";
+	private int Add() {
+		ensureNoNegativeNumbers();
+		return getNumbers().sum();
+	}
 
-        for(String number : numbers){
-        	if(toInt(number) < 0){
-        		if(negString.equals(""))
-        			negString = number;
-        		else
-        			negString += ("," + number);
-        	}
-        	if(toInt(number) < 1000)
-		    	total += toInt(number);
+	private void ensureNoNegativeNumbers() {
+		String negativeNumberSequence = getNumbers().filter(n -> n < 0)
+				.mapToObj(Integer::toString)
+				.collect(Collectors.joining(","));
+		if (!negativeNumberSequence.isEmpty()) {
+			throw new IllegalArgumentException("negatives not allowed: " + negativeNumberSequence);
 		}
+	}
 
-		if(!negString.equals("")){
-			throw new IllegalArgumentException("Negatives not allowed: " + negString);
+	private IntStream getNumbers() {
+		if (numbers.isEmpty()) {
+			return IntStream.empty();
+		} else {
+			return Stream.of(numbers.split(delimiter))
+					.mapToInt(Integer::parseInt)
+					.map(n -> n % 1000);
 		}
+	}
 
-		return total;
-    }
+	public static int Add(String input) {
+		return parseInput(input).Add();
+	}
 
+	private static StringCalculator parseInput(String input) {
+		if (input.startsWith("//")) {
+			String[] headerAndNumberSequence = input.split("\n", 2);
+			String delimiter = parseDelimiter(headerAndNumberSequence[0]);
+			return new StringCalculator(delimiter, headerAndNumberSequence[1]);
+		} else {
+			return new StringCalculator(",|\n", input);
+		}
+	}
+
+	private static String parseDelimiter(String header) {
+		String delimiter = header.substring(2);
+		if (delimiter.startsWith("[")) {
+			delimiter = delimiter.substring(1, delimiter.length() - 1);
+		}
+		return Stream.of(delimiter.split("]\\["))
+				.map(Pattern::quote)
+				.collect(Collectors.joining("|"));
+	}
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,36 +1,19 @@
 package calculator;
-
-import static org.junit.Assert.assertEquals;
-
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.junit.Assert.assertThat;
 
 public class StringCalculatorShould {
 
-    @Test
-    public void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
+	@SuppressWarnings("deprecation")
+	@Test
+	public void sumsEmptyStringTo0() {
+		assertThat(StringCalculator.Add(""), is(0));
+	}
 
-    @Test
-    public void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
-    
-    @Test
-    public void string_with_Two_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(3, stringCalculator.add("1,2"));
-    }
-    
-
-    @Test
-    public void testNewLine(){
-    	//second test for new line
-        StringCalculator stringCalculator = new StringCalculator();
-    	assertEquals(6, stringCalculator.add("1\n2,3"));
-    }
-
-        
+	
 }
